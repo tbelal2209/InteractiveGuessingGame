1a8b5543260c23b128873eb7ba5c8173beaf90b0
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-10 20:22:29 -0400

    Update Main.java

--- src/TazwarBelal/Main.java
+++ src/TazwarBelal/Main.java
@@ -9,12 +9,24 @@ public class Main {
         Scanner input = new Scanner(System.in) ;
         System.out.print("What is your name?");
         String name = input.nextLine();
-        System.out.println("Hello"+" "+name);
-        System.out.print("Guess My Number between 1 and 10");
+        System.out.println("Hello"+" "+name+" "+"Would you like type 1 or type 2");
+        String Answer = input.nextLine();
+        if(Answer.equals("1") ) {
+            System.out.print("Guess My Number between 1 and 10");
 
-        while(!(input.nextLine().equals("3"))){
-            System.out.println("Guess my number again"+" "+ name);
+            while (!(input.nextLine().equals("3"))) {
+                System.out.println("Guess my number again" + " " + name);
+            }
+            System.out.println("You guessed my number" + " " + name);
         }
-        System.out.println("You guessed my number"+" "+name);
+        else if (Answer.equals("2")){
+            System.out.print("Guess My Number between 1 and 100");
+            while (!(input.nextLine().equals("89"))) {
+                System.out.println("Guess my number again" + " " + name);
+            }
+            System.out.println("You guessed my number" + " " + name);
+
+        }
+
     }
 }
