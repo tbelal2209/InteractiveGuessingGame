c73ef23210ff328b4dd805eaa70b4125ff411d09
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-10 20:01:51 -0400

    Update Main.java

--- src/TazwarBelal/Main.java
+++ src/TazwarBelal/Main.java
@@ -5,17 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int x = Math.random(0,10);
+        // write your code here
         Scanner input = new Scanner(System.in) ;
         System.out.print("What is your name?");
         String name = input.nextLine();
-        System.out.println("Hello"+name);
-        System.out.print("Guess My Number");
+        System.out.println("Hello"+" "+name);
+        System.out.print("Guess My Number between 1 and 10");
 
-        while(!(x==3)){
-            System.out.println("Guess my number again"+name);
+        while(!(input.nextLine().equals("3"))){
+            System.out.println("Guess my number again"+" "+ name);
         }
-        System.out.println("You guessed mu number"+name);
+        System.out.println("You guessed my number"+" "+name);
     }
 }
